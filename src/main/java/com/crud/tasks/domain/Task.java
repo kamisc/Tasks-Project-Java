76a61db81e49e0b63cc6507d60ad3647feedc00d
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-07-15 21:34:11 +0200

    19.2. Connection with the database

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,15 +2,26 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * Author Kamil Seweryn
  */
 
-@Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
