48cc12313b03c083807b2822d985c8c9665a8f7f
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-07-31 21:31:01 +0200

    22.2. Rest Template

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
